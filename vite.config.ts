baef2a5e0a3aaf4dc5ff9d29c11a438a2e0c8d7a
Kermout Ayoub <ayoub.kermout@etu.usthb.dz>
2024-02-27 13:24:30 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, type UserConfig } from "vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { builderDevTools } from "@builder.io/dev-tools/vite";
+
+export default defineConfig((): UserConfig => {
+  return {
+    plugins: [builderDevTools(), qwikCity(), qwikVite(), tsconfigPaths()],
+    server: {
+      headers: {
+        "Cache-Control": "public, max-age=0",
+      },
+    },
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+  };
+});
